fcc1c152085935224969901a39764a5ed71fec7a
sarL3y <matt.tapper@gmail.com>
2019-09-17 23:59:01 -0700

    Style updates // Bio v1

--- src/components/About.js
+++ src/components/About.js
@@ -9,15 +9,22 @@ export default function About(props) {
 
     return (
         <div className="about-container">
-            <header className="about-header">
+            {/* <header className="about-header">
                 <div>
                     <h2>About</h2>
                 </div>
-            </header>
+            </header> */}
 
             <section className="about">
-                <div className="about-item">
+                <div className="headshot-container">
+                    <div className="headshot">
 
+                    </div>
+                </div>
+                <div className="bio-container">
+                    <p>I’m a web developer based in Los Angeles, and I’m excited to join your team. I love seeing projects come to life, from the first concept in a wireframe to that first spark of personality when it comes to life on the page.</p>
+                    <p>For the past 6 years, I’ve worked in the television industry on teams that produce some of the biggest reality shows in the world. My problem-solving skills and desire to connect more to a user’s experiences led me to programming and web development.</p>
+                    <p>It’s been a year since my first commit. Since then, I’ve been absorbing the latest JavaScript frameworks (React Hooks!), building a freelance client base for personal sites, and forging my new path in this exciting industry. </p>
                 </div>
             </section>
         </div>
