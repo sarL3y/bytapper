fb3d78abee1b37aa54b02ab9f1cd39df95648170
sarL3y <matt.tapper@gmail.com>
2019-09-29 23:56:03 -0700

    Fix serverless functionality (update fetch url)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,7 +30,7 @@ export default function Contact(props) {
 
         axios({
             method: "POST", 
-            url:"/submitForm",
+            url:"https://bytapper.com/submitForm",
             // url: `${window.location.href}/submitForm`,
             data: {...contactData}
         }).then((response)=>{
