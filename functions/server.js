38b67e6a2e0ef3bbd122417f5f0f4b25d2e92c59
sarL3y <matt.tapper@gmail.com>
2019-09-30 00:50:55 -0700

    Fix serverless functionality (update server.js)

--- functions/server.js
+++ functions/server.js
@@ -5,17 +5,21 @@ const gmailHelpers = require('./support/gmail.js');
 
 const app = express();
 const bodyParser = require('body-parser');
+const router = express.Router();
 // const port = process.env.PORT || 3000;
 
 // app.use(express.json());
-app.use(bodyParser);
+app.use(bodyParser.json());
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
+app.use('/.netlify/functions/server', router);
 
-app.post('/submitForm', function(req, res) {
+router.post('/test', (req, res) => res.json({ postBody: req.body }));
+
+router.post('/submitForm', function(req, res) {
     const messageOptions = {
         from: req.body.email,
         to: 'bytapper@gmail.com',
@@ -32,4 +36,5 @@ app.post('/submitForm', function(req, res) {
 // Spin it up
 // app.listen(port, console.log(`Listening on port: ${port}`));
 
+module.exports = app;
 module.exports.handler = serverless(app);
