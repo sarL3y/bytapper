1225a2952eba9e7009fe3675ccfa66327e65e330
sarL3y <matt.tapper@gmail.com>
2019-09-30 00:06:47 -0700

    Fix serverless functionality (update functions server.js)

--- functions/server.js
+++ functions/server.js
@@ -8,11 +8,11 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post('/submitForm', function(req, res) {
     const messageOptions = {
